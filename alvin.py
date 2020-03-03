6b22d1e02c6b75877522dc76bf46fac2419b0eed
dragon-alvin1m <alvin.riget.dk@gmail.com>
2020-03-03 18:28:57 +0100

    ny version

--- alvin.py
+++ alvin.py
@@ -1,6 +1,10 @@
 # Alvins fil. GÅ UD!!!! 
 import turtle 
-size=40
-for n in range(1,5):
-    turtle.forward(size)
-    turtle.left(90)
+
+
+def firkant(size=40):
+    for n in range(1,5):
+        turtle.forward(size)
+        turtle.left(90)
+
+firkant()
