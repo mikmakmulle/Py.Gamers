543f4a797c5785c76213bd61950843fbe257b86a
isvaffel100 <61467634+isvaffel100@users.noreply.github.com>
2020-03-03 16:49:47 +0100

    totinoes totinoes hot pizza rolls totinoes totinoes everybody is talking about totinoes totinoes hot pizza rolls

--- isvaffel100.py
+++ isvaffel100.py
@@ -1,4 +1,5 @@
 #dette er en turtle stjerne lavet er den seje andreas tropp hag yaaaasss
+#totinoes totinoes hot pizza rolls totinoes totinoes everybody is talking about totinoes totinoes hot pizza rolls totinoes totinoes everybody is talking about totinoes totinoes hot pizza rolls
 from turtle import *
 
 color('red', 'yellow')
