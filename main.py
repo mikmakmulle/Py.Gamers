2a02cfd6d41fc38fcc151a40653ce5c1d2c2a08b
paragi <simon.riget@gmail.com>
2020-03-05 16:14:30 +0100

    .

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from simon   import *  # Hent Simons funktioenr
 
 import turtle # her importerer vi hele turtle modulet
 
-#turtle.speed("fastest")
+turtle.speed("fastest")
 #turtle.color(0.4,0,1)
 #turtle.pensize(4)
 
@@ -28,6 +28,23 @@ def flower():
     turtle.left(36)
 
 
-flower()
+# flower()
+
+def hus(size=500):
+  h = size/2
+
+  firkant(h)
+  move(-h/10,-h)
+  trekant(h * 1.2)
+  move(h/8+h/10,h/2)
+  firkant(h/6)
+  move(h/1.8,0)
+  firkant(h/6)
+
+hus()
+
 
 input()
+
+# 1. Beregn høje af figur, så den bliver size
+# 2. byg hus
