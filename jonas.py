04a07c600d667f8b02d160d70282eaa1b7e8babd
Cygwingthecoder <61467733+Cygwingthecoder@users.noreply.github.com>
2020-03-10 16:55:03 +0100

    det virker ikke martin

--- jonas.py
+++ jonas.py
@@ -1,6 +1,4 @@
 import turtle
-
 def sekskant(size=40):
-    for n in range(0,6):
-        turtle.forward(size)
-        turtle.right(60)
+    turtle.forward(size)
+    turtle.right(60)
---
+++ lav sjov her(;.py
@@ -0,0 +1,8 @@
+import turtle# M.I.S
+turtle.speed("fastest")# M.I.S
+turtle.pencolor("blue")# M.I.S
+
+for n in range (9999999999999999)# M.I.S
+    turtle.right(90)# M.I.S
+    turtle.forward(n-9999)# M.I.S
+    #lavet at jonas M.I.S
