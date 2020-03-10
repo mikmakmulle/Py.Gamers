45a59ec0ee34434f061702ebc159d91d76b8df6a
cp-henrik <henrik@hj-privat.localdomain>
2020-03-10 17:46:51 +0100

    ss

--- henrik.py
+++ henrik.py
@@ -5,4 +5,15 @@ def kantet(size=40):
     for n in range(1,6):
         turtle.forward(size)
         turtle.right(72)
-kantet()
+
+
+def sol():
+#    from turtle import *
+    color('red', 'yellow')
+    begin_fill()
+    while True:
+        forward(100)
+        left(170)
+        if abs(pos()) < 1:
+            break
+    end_fill()
