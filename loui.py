8beea0d951fa7118c824f3be121da9ed5796dcb5
yeetloui <holstebroeloui@gmail.com>
2020-03-10 16:27:54 +0100

    raktangel

---
+++ loui.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+
+def raktangel(height,width):
+    turtle.forward(width)
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.left(90)
+    turtle.forward(width)
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.left(90)
+
+
+
+
