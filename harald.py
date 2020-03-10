62e217b172f44bed57fcdb96741e58d9939f8f4e
haaarh <harald@eystein.dk>
2020-03-10 18:29:03 +0100

    ny blomst

--- harald.py
+++ harald.py
@@ -1,6 +1,31 @@
 import turtle
+from loui   import *
+from andreas import *
 
-def trekant(size=40):
-    for n in range(1,4):
-        turtle.forward(size)
-        turtle.left(120)
+if __name__ == '__main__':
+    def trekant(size=40):
+        for n in range(1,4):
+            turtle.forward(size)
+            turtle.left(120)
+            
+def godblomst():
+    circle (30)
+    turtle.penup()
+    turtle.goto(0,60)
+    turtle.pendown()
+    circle (75)
+    turtle.penup()
+    turtle.goto(-30,30)
+    turtle.left(90)
+    turtle.pendown()
+    circle (75)
+    turtle.penup()
+    turtle.goto(0,0)
+    turtle.left(90)
+    turtle.pendown()
+    circle (75)
+    turtle.penup()
+    turtle.goto(30,30)
+    turtle.left(90)
+    turtle.pendown()
+    circle (75)
