e5339de7fe279e0430e45ff16b03d50c06ddc68f
cp-henrik <henrik@hj-privat.localdomain>
2020-03-10 18:05:52 +0100

    sol virker :-)

--- henrik.py
+++ henrik.py
@@ -6,13 +6,18 @@ def kantet(size=40):
         turtle.forward(size)
         turtle.right(72)
 
-
+    
 def sol():
     turtle.color('red', 'yellow')
     turtle.begin_fill()
     while True:
         turtle.forward(100)
         turtle.left(170)
-        if abs(pos()) < 1:
+        if abs(turtle.pos()) < 1:
             break
     turtle.end_fill()
+
+
+if __name__ == "__main__":
+
+    sol()
