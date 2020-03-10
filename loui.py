c5e2d91d1a6627f7154af419050e40258837a9eb
yeetloui <holstebroeloui@gmail.com>
2020-03-10 18:17:59 +0100

    flagstang

--- loui.py
+++ loui.py
@@ -16,4 +16,54 @@ if __name__ == "__main__":
     rektangel(250,25)
     turtle.left(90)
     turtle.forward(250)
-    #turtle.
+    turtle.right(90)
+    turtle.forward(150)
+    turtle.right(90)
+    turtle.forward(75)
+    turtle.right(90)
+    turtle.forward(125)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(60)
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.penup()
+    turtle.goto(0,0)
+    turtle.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+input()
