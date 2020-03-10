9a27bcf8f9af0e202e51e5051cedecd7e5feaf43
yeetloui <holstebroeloui@gmail.com>
2020-03-10 17:21:35 +0100

    .

--- loui.py
+++ loui.py
@@ -12,4 +12,8 @@ def rektangel(height,width):
     turtle.forward(height)
     turtle.left(90)
 
-rektangel(250,25)
+if __name__ == "__main__":
+    rektangel(250,25)
+    turtle.left(90)
+    turtle.forward(250)
+    #turtle.
