fa6b184b76bce0dfa2885650858fb69cfd7f02dc
dragon-alvin1m <alvin.riget.dk@gmail.com>
2020-03-10 18:10:08 +0100

    .

--- alvin.py
+++ alvin.py
@@ -6,8 +6,24 @@
 import turtle 
 
 
+from andreas import *  # Hent Andreas funktioenr
+from jonas   import *  # Hent Jonas funktioenr
+from harald  import *  # Hent Haralds funktioenr
+from henrik  import *  # Hent Henriks funktioenr
+from martin  import *  # Hent Martins funktioenr
+from simon   import *  # Hent Simons funktioenr
+from rektangel  import *
+
 def firkant(size=40):
     for n in range(1,5):
         turtle.forward(size)
         turtle.left(90)
 
+
+if __name__ == '__main__':
+    circle(100)
+
+    
+    turtle.circle(20)
+
+    input()
